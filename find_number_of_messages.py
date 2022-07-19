22098b9b300cc29282cb3085b4d6523d1f8d4581
KarimovProject <abdullatifkarimov772@gmail.com>
2022-07-19 11:21:30 -0400

    number message

--- find_number_of_messages.py
+++ find_number_of_messages.py
@@ -1,9 +1,5 @@
-from email import message
 import json
-
-from numpy import s_
 from read_data import read_data
-
 def find_number_of_messages(data: dict)->int:
     """
     Get the total number of messages.
